4bd9f2000d6088bd5671a8435c5cdd75b8b199bf
Demur Rumed <gunkmute@gmail.com>
2015-10-03 04:04:47 +0000

    Partially translated mbrubeck's box/layout engine

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ var vm = require("vm");
 var domcore = require("./domcore");
 var renderer = require("./renderer");
 domcore.glinit();
-var page = new renderer.Page();
+var page = new renderer.Page(800, 600);
 page.parseLink("file:///wb.html");
 setInterval(()=>{page.step()}, 100);
--- renderer.js
+++ renderer.js
@@ -5,12 +5,15 @@ var p5 = require('parse5');
 var css = require('css/parse');
 var vm = require('vm');
 var domcore = require('./domcore');
-htmlparser = new p5.Parser();
-function Page(){
+var htmlparser = new p5.Parser();
+function Page(width, height){
 	this.ctx = null;
 	this.styles = null;
 	this.head = null;
 	this.body = null;
+	this.content = new Float32Array([0,0,width,height]);
+	this.padding = this.border = this.margin = new Float32Array(4);
+	this.style = {};
 }
 var cssparseopt = {silent:true};
 function specificity(x){
@@ -57,10 +60,7 @@ var processHandler = {
 };
 var drawHandler = {
 	'#text':function(node){
-		var text = node.value;
-		for(var i=0; i<text.length; i++){
-
-		}
+		//rendertext(node.value);
 	},
 };
 function processCore(node){
@@ -84,12 +84,60 @@ function selectorMatch(selector, node){
 		selector[0] == '.' ? node.attrs.class && node.attrs.class.split(' ').indexOf(selector.slice(1)) != -1 :
 		selector == node.tagName;
 }
-function styleCore(node, inherit){
+function calcBlockWidth(parent){
+	var style = node.style;
+	var width = style.width || "auto";
+	this.content = new Float32Array(4);
+	this.padding = new Float32Array(4);
+	this.border = new Float32Array(4);
+	this.margin = new Float32Array(4);
+	var total = parseInt(this.width,10);
+	["padding", "border", "margin"].forEach(prop => {
+		var default = style[prop];
+		["left","top","right","bottom"].forEach((dim, idx) => {
+			var val = this[prop][idx] = style[prop+"-"+dim] || default || 0;
+			if (!(idx&1))total += parseInt(val,10);
+		});
+	});
+	var parentWidth = parent.content[2] - parent.content[0];
+	if (width != "auto" && total > parentWidth){
+		if (style["margin-left"] == "auto") this.margin[0] = 0;
+		if (style["margin-right"] == "auto") this.margin[2] = 0;
+	}
+	var underflow = parentWidth - total,
+		auto = (width == "auto")<<2|(style["margin-left"] == "auto")<<1|(style["margin-right"] == "auto");
+	if (!auto){
+		this.margin[2] = parseInt(style["margin-right"],10);
+	}else if (auto == 1){
+		this.margin[2] = underflow;
+	}else if (auto == 2){
+		this.margin[0] = underflow;
+	}else if (auto == 3){
+		this.margin[0] = this.margin[2] = underflow/2;
+	}else if (auto&4){
+		if (auto&1) this.margin[2] = 0;
+		if (auto&2) this.margin[0] = 0;
+		if (underflow >= 0) width = underflow;
+		else{
+			width = 0;
+			this.margin[2] += underflow;
+		}
+	}
+	this.content[2] = parseInt(width,10);
+	this.padding[0] = parseInt(style["padding-left"],10);
+	this.padding[2] = parseInt(style["padding-right"],10);
+	this.border[0] = parseInt(style["border-left"],10);
+	this.border[2] = parseInt(style["border-right"],10);
+	if (style["margin-left"] != "auto") this.margin[0] = parseInt(style["margin-left"],10);
+	if (style["margin-right"] != "auto") this.margin[2] = parseInt(style["margin-right"],10);
+}
+function nodeHeight(node){
+	return node.content[3] + node.padding[1] + node.padding[3] + node.border[1] + node.border[3] + node.margin[1] + node.margin[3];
+}
+function styleCore(parent, node){
 	var mysty = this.styles.filter(style => style.selectors.every(selector => selectorMatch(selector, node)));
 	node.style = {};
-	if (inherit){
-		for(var key in inherit) node.style[key] = inherit[key];
-	}
+	for (var key in parent.style) node.style[key] = parent.style[key];
 	mysty.forEach(style => (node.style[style.property] = style.value));
 	if (node.attrs.style){
 		node.attrs.style.split(';').forEach((x) => {
@@ -97,10 +145,13 @@ function styleCore(node, inherit){
 			if (~a) node.style[x.slice(0,a)] = x.slice(a+1);
 		});
 	}
-	node.childNodes.forEach(styleCore.bind(this, node.style));
+	calcBlockWidth.call(node, parent);
+	node.childNodes.forEach(styleCore.bind(this, node));
+	if (node.style.height && node.style.height != "auto") node.content[3] = parseInt(node.style.height,10);
+	parent.content[3] += nodeHeight(node);
 }
 Page.prototype.restyle = function(){
-	styleCore.call(this, this.body);
+	styleCore.call(this, this, this.body);
 }
 function drawCore(node){
 	if (name in drawHandler) drawHandler[name].call(this, node);
