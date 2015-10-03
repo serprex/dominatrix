90489a5905acbb643a4c525a3b96a23434ac8ecc
Demur Rumed <gunkmute@gmail.com>
2015-10-03 15:46:28 +0000

    Calc pos, avoid NaNs, settle a bit on how to go about implementing domcore API

--- main.js
+++ main.js
@@ -5,5 +5,5 @@ var domcore = require("./domcore");
 var renderer = require("./renderer");
 domcore.glinit();
 var page = new renderer.Page(800, 600);
-page.parseLink("file:///wb.html");
-setInterval(()=>{page.step()}, 100);
+if (typeof exports !== 'undefined') exports.page = page;
+page.parseLink("file:///wb.html").then(() => setInterval(() => {page.render()}, 33));
--- renderer.js
+++ renderer.js
@@ -2,23 +2,24 @@
 var fs = require('fs');
 var http = require('http');
 var p5 = require('parse5');
-var css = require('css/parse');
+var css = require('css/lib/parse');
 var vm = require('vm');
 var domcore = require('./domcore');
 var htmlparser = new p5.Parser();
-function Page(width, height){
+function Page(width){
 	this.ctx = null;
 	this.styles = null;
 	this.head = null;
 	this.body = null;
-	this.content = new Float32Array([0,0,width,height]);
+	this.domcache = {};
+	this.content = new Float32Array([0,0,width,0]);
 	this.padding = this.border = this.margin = new Float32Array(4);
 	this.style = {};
 }
 var cssparseopt = {silent:true};
 function specificity(x){
 	var a = 0;
-	x.selectors.forEach((s) => {
+	x.selectors.forEach(s => {
 		if (s && s != '*'){
 			while (s[0] == '*')s=s.slice(1);
 			a += 1<<(s[0] == '#' ? 16 : s[0]=='.' ? 8 : 0);
@@ -52,27 +53,24 @@ var processHandler = {
 		}
 	},
 	script:function(node){
-		vm.runInContext(node.childNodes[0], this.ctx);
+		//vm.runInContext(node.childNodes[0].value, this.ctx);
 	},
 	style:function(node){
-		this.styles.push(cssparse(node.childNodes[0]));
+		this.styles.push(cssparse(node.childNodes[0].value));
 	}
-};
-var drawHandler = {
-	'#text':function(node){
-		//rendertext(node.value);
-	},
 };
 function processCore(node){
 	var name = node.nodeName;
 	if (name in processHandler) processHandler[name].call(this, node);
-	else node.childNodes.forEach(processCore, this);
+	else if (node.childNodes) node.childNodes.forEach(processCore, this);
 }
 Page.prototype.process = function(dom){
 	this.ctx = vm.createContext();
 	this.styles = [];
-	this.head = dom.childNodes[0].childNodes[0];
-	this.body = dom.childNodes[0].childNodes[1];
+	dom.childNodes[0].childNodes.forEach(node => {
+		if (node.nodeName == 'head') this.head = node;
+		else if (node.nodeName == 'body') this.body = node;
+	});
 	processCore.call(this, dom);
 	this.restyle();
 }
@@ -84,19 +82,22 @@ function selectorMatch(selector, node){
 		selector[0] == '.' ? node.attrs.class && node.attrs.class.split(' ').indexOf(selector.slice(1)) != -1 :
 		selector == node.tagName;
 }
+function parsePx(str){
+	return str == "auto" ? 0 : parseInt(str,10);
+}
 function calcBlockWidth(parent){
-	var style = node.style;
+	var style = this.style;
 	var width = style.width || "auto";
 	this.content = new Float32Array(4);
 	this.padding = new Float32Array(4);
 	this.border = new Float32Array(4);
 	this.margin = new Float32Array(4);
-	var total = parseInt(this.width,10);
+	var total = parsePx(width);
 	["padding", "border", "margin"].forEach(prop => {
-		var default = style[prop];
+		var defval = style[prop];
 		["left","top","right","bottom"].forEach((dim, idx) => {
-			var val = this[prop][idx] = style[prop+"-"+dim] || default || 0;
-			if (!(idx&1))total += parseInt(val,10);
+			var val = this[prop][idx] = style[prop+"-"+dim] || defval || 0;
+			if (!(idx&1))total += parsePx(val);
 		});
 	});
 	var parentWidth = parent.content[2] - parent.content[0];
@@ -107,7 +108,7 @@ function calcBlockWidth(parent){
 	var underflow = parentWidth - total,
 		auto = (width == "auto")<<2|(style["margin-left"] == "auto")<<1|(style["margin-right"] == "auto");
 	if (!auto){
-		this.margin[2] = parseInt(style["margin-right"],10);
+		this.margin[2] = parsePx(style["margin-right"]);
 	}else if (auto == 1){
 		this.margin[2] = underflow;
 	}else if (auto == 2){
@@ -123,19 +124,29 @@ function calcBlockWidth(parent){
 			this.margin[2] += underflow;
 		}
 	}
-	this.content[2] = parseInt(width,10);
-	this.padding[0] = parseInt(style["padding-left"],10);
-	this.padding[2] = parseInt(style["padding-right"],10);
-	this.border[0] = parseInt(style["border-left"],10);
-	this.border[2] = parseInt(style["border-right"],10);
-	if (style["margin-left"] != "auto") this.margin[0] = parseInt(style["margin-left"],10);
-	if (style["margin-right"] != "auto") this.margin[2] = parseInt(style["margin-right"],10);
+	this.content[2] = parsePx(width);
+	this.padding[0] = parsePx(style["padding-left"] || style["padding"] || 0);
+	this.padding[2] = parsePx(style["padding-right"] || style["padding"] || 0);
+	this.border[0] = parsePx(style["border-left"] || style["border"] || 0);
+	this.border[2] = parsePx(style["border-right"] || style["border"] || 0);
+}
+function calcBlockPos(parent){
+	var style = this.style;
+	this.padding[1] = parsePx(style["padding-top"] || style["padding"] || 0);
+	this.padding[3] = parsePx(style["padding-bottom"] || style["padding"] || 0);
+	this.border[1] = parsePx(style["border-top"] || style["border"] || 0);
+	this.border[3] = parsePx(style["border-bottom"] || style["border"] || 0);
+	this.margin[1] = parsePx(style["margin-top"] || style["margin"] || 0);
+	this.margin[3] = parsePx(style["margin-bottom"] || style["margin"] || 0);
+	this.content[0] = parent.content[0] + this.padding[0] + this.border[0] + this.margin[0];
+	this.content[1] = parent.content[1] + parent.content[3] + parent.padding[1] + parent.border[1] + parent.margin[1];
 }
 function nodeHeight(node){
 	return node.content[3] + node.padding[1] + node.padding[3] + node.border[1] + node.border[3] + node.margin[1] + node.margin[3];
 }
 function styleCore(parent, node){
-	var mysty = this.styles.filter(style => style.selectors.every(selector => selectorMatch(selector, node)));
+	if (!node.attrs) return;
+	var mysty = this.styles.filter(style => style.selectors && style.selectors.every(selector => selectorMatch(selector, node)));
 	node.style = {};
 	for (var key in parent.style) node.style[key] = parent.style[key];
 	mysty.forEach(style => (node.style[style.property] = style.value));
@@ -146,16 +157,13 @@ function styleCore(parent, node){
 		});
 	}
 	calcBlockWidth.call(node, parent);
+	calcBlockPos.call(node, parent);
 	node.childNodes.forEach(styleCore.bind(this, node));
-	if (node.style.height && node.style.height != "auto") node.content[3] = parseInt(node.style.height,10);
+	if (node.style.height && node.style.height != "auto") node.content[3] = parsePx(node.style.height);
 	parent.content[3] += nodeHeight(node);
 }
 Page.prototype.restyle = function(){
 	styleCore.call(this, this, this.body);
-}
-function drawCore(node){
-	if (name in drawHandler) drawHandler[name].call(this, node);
-	else node.childNodes.forEach(drawCore, this);
 }
 Page.prototype.parseLink = function(url){
 	return new Promise((resolve, reject) => {
@@ -171,12 +179,6 @@ Page.prototype.parse = function(html){
 	this.process(htmlparser.parse(html));
 }
 Page.prototype.render = function(){
-	drawCore.call(this, this.body);
+	domcore.glloop(this.body, this.domcache);
 }
 exports.Page = Page;
-exports.draw = function(doc){
-	drawCore.call(doc, doc.body);
-}
-exports.step = function(){
-	domcore.glloop();
-}
